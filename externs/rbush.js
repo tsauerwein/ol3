bc4cf69801f60cec9ee97fa381760a439b723fc4
tsauerwein <tobias.sauerwein@camptocamp.com>
2014-10-23 09:52:24 +0200

    WIP: include rbush via externs

---
+++ externs/rbush.js
@@ -0,0 +1,13 @@
+/**
+ * @externs
+ */
+
+/**
+ * @constructor
+ * @param {number=} opt_maxEntries Max entries.
+ * @see https://github.com/mourner/rbush
+ * @struct
+ * @template T
+ */
+var rbush = function(opt_maxEntries) {};
+
